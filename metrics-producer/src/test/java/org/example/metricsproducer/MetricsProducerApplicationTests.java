648ed1a8e12e7e5046f32da901c1b0ff8e969f7e
adziri <liubov.nikita@gmail.com>
2024-03-20 19:28:26 +0100

    init

---
+++ metrics-producer/src/test/java/org/example/metricsproducer/MetricsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.metricsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
